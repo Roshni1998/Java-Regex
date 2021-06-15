cd1f04b5b9a2a95891a535172f9adac285b65491
Roshni1998 <roshnimali847@gmail.com>
2021-06-15 20:47:20 +0530

    Added: Email id UC-4

--- EmailValidation.java
+++ EmailValidation.java
@@ -19,14 +19,14 @@ public class EmailValidation {
 
 	public static boolean isValidEmailId(String pinCode) {
 		// Regex to check valid Email Id
-		String regex = "^([.a-zA-Z]{2,4})$";
+		String regex = "^([a-zA-Z0-9.]+)([0-9a-zA-Z]{0,1})$";
 		Pattern p = Pattern.compile(regex);
 		Matcher m = p.matcher(pinCode);
 		return m.matches();
 	}
 
 	public static void main(String[] args) {
-		String id = ".co";
+		String id = "abc.xyz";
 		System.out.println("Email Id : " + isValidEmailId(id));
 
 	}
