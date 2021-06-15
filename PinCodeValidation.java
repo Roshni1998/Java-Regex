ac3d13013c4d11e5452c921f004987df2ae787b6
Roshni1998 <roshnimali847@gmail.com>
2021-06-15 14:40:02 +0530

    Added: Pin Code UC-1

--- PinCodeValidation.java
+++ PinCodeValidation.java
@@ -1,13 +1,22 @@
 package com.bridgelabz.regularexpression;
+
+import java.util.regex.Pattern;
+
 /**
  * 
  * @author ROSHNI
- * Welcome Message
+ * Regex validation for PIN Code
  */
 
 public class PinCodeValidation {
-	
-	public static void main(String[] args) {
-		System.out.println("Welcome to Java Regex");
+
+	/**
+	 * 
+	 * @param PinCode
+	 * @return boolean validating for a PIN Code
+	 */
+
+	public boolean validPinCode(String pincode) {
+		return Pattern.matches("^[0-9]{6}$", pincode);
 	}
 }
---
+++ PinCodeValidationTest.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.regularexpression;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+/**
+ * 
+ * @author ROSHNI
+ * Test Case for Pin Code validation
+ *
+ */
+
+public class PinCodeValidationTest {
+
+	@Test
+	public void passThePinCodeValidationItShouldReturnValidOrNot() {
+		PinCodeValidation code = new PinCodeValidation();
+		assertTrue(code.validPinCode("400088"));
+		assertTrue(code.validPinCode("056432"));
+		assertFalse(code.validPinCode("40rt88"));
+		assertFalse(code.validPinCode("40008823"));
+	}
+}
