614eec6b6f2a84f635560a5260e14cf1e617834f
Roshni1998 <roshnimali847@gmail.com>
2021-06-15 16:27:16 +0530

    Added: Pin Code UC-3

--- PinCodeValidationTest.java
+++ PinCodeValidationTest.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.regularexpression;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 /**
  * 
@@ -15,10 +15,11 @@ public class PinCodeValidationTest {
 	@Test
 	public void passThePinCodeValidationItShouldReturnValidOrNot() {
 		PinCodeValidation code = new PinCodeValidation();
-		assertTrue(code.validPinCode("400088"));
-		assertTrue(code.validPinCode("056432"));
+		assertFalse(code.validPinCode("056432"));
 		assertFalse(code.validPinCode("40rt88"));
-		assertFalse(code.validPinCode("40008823"));
 		assertFalse(code.validPinCode("A400088"));
+		assertFalse(code.validPinCode("T000882"));
+		assertFalse(code.validPinCode("e00088"));
+		assertFalse(code.validPinCode("70088b"));
 	}
 }
