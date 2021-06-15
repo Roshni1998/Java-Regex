f2de7dd8e34128954b6294bf90b869c325559bb5
Roshni1998 <roshnimali847@gmail.com>
2021-06-15 14:33:52 +0530

    Added: Welcome Message

---
+++ PinCodeValidation.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.regularexpression;
+/**
+ * 
+ * @author ROSHNI
+ * Welcome Message
+ */
+
+public class PinCodeValidation {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Regex");
+	}
+}
