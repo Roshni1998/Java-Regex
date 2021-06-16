ae2ddd5e168a3b1a647c54d275135b70932cb2bb
Roshni1998 <roshnimali847@gmail.com>
2021-06-16 18:06:30 +0530

    Added: User Registration UC-2

--- UserRegistration.java
+++ UserRegistration.java
@@ -13,11 +13,20 @@ public class UserRegistration {
 	 * 
 	 * @param firstName
 	 * @return boolean
-	 * First name starts with Cap and has minimum 3 characters
 	 */
+	
+	/*UC-1*/
+	//First name starts with Cap and has minimum 3 characters
+	
 	public static boolean isValidFirstName(String firstName) {
 		return Pattern.matches("^[A-Z][a-z]{3,15}$", firstName);
-		
+	}
+	
+	/*UC-2*/
+	// Last name starts with Cap and has minimum 3 characters
+	
+	public static boolean isValidLastName(String lastName) {
+		return Pattern.matches("^[A-Z][a-z]{3,15}$", lastName);
 	}
 	
 	public static void main(String[] args) {
@@ -25,7 +34,10 @@ public class UserRegistration {
 		System.out.println("Enter the First Name : ");
 		Scanner in = new Scanner(System.in);
 		String firstName = in.nextLine();
-		System.out.println("Is First Name Valid ? : "+isValidFirstName(firstName));
+		System.out.println("Enter the Last Name : ");
+		String lastName = in.nextLine();
+		System.out.println("Is First Name Valid ? : "+ isValidFirstName(firstName));
+		System.out.println("Is Last Name Valid ? : "+ isValidLastName(lastName));
 	}
 
 }
