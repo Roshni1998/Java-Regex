1246416462dd197db5b37b489792c7ae6fc27c41
Roshni1998 <roshnimali847@gmail.com>
2021-06-16 18:12:02 +0530

    Added: User Registration UC-3

--- UserRegistration.java
+++ UserRegistration.java
@@ -19,14 +19,20 @@ public class UserRegistration {
 	//First name starts with Cap and has minimum 3 characters
 	
 	public static boolean isValidFirstName(String firstName) {
-		return Pattern.matches("^[A-Z][a-z]{3,15}$", firstName);
+		return Pattern.matches("^[A-Z]{1}[a-z_-]{3,15}$", firstName);
 	}
 	
 	/*UC-2*/
 	// Last name starts with Cap and has minimum 3 characters
 	
 	public static boolean isValidLastName(String lastName) {
-		return Pattern.matches("^[A-Z][a-z]{3,15}$", lastName);
+		return Pattern.matches("^[A-Z]{1}[a-z_-]{3,15}$", lastName);
+	}
+	
+	/*UC-3*/
+	//Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
+	public static boolean isValidEmailId(String emailId) {
+		return Pattern.matches("^([a-zA-Z0-9.]+)([0-9a-zA-Z]{0,1}@([a-zA-Z0-9.]+([a-zA-Z]{2,3}))+([a-z]{0,2}))$", emailId);
 	}
 	
 	public static void main(String[] args) {
@@ -36,8 +42,11 @@ public class UserRegistration {
 		String firstName = in.nextLine();
 		System.out.println("Enter the Last Name : ");
 		String lastName = in.nextLine();
-		System.out.println("Is First Name Valid ? : "+ isValidFirstName(firstName));
-		System.out.println("Is Last Name Valid ? : "+ isValidLastName(lastName));
+		System.out.println("Enter Your Email ID : ");
+		String emailId = in.nextLine();
+		System.out.println("Is First Name valid ? : "+isValidFirstName(firstName));
+		System.out.println("Is Last Name valid : "+isValidLastName(lastName));
+		System.out.println("Is Email ID valid : "+isValidEmailId(emailId));
 	}
 
 }
